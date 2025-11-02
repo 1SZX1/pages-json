b5a2f61638beb81870d95f2ab6ce76355fa1955d
edwinhuish <edwinhuish@gmail.com>
2025-11-02 20:32:27 +0800

    chore: 拆分 pages  和 subPackages 测试

--- test/files.spec.ts
+++ test/files.spec.ts
@@ -13,14 +13,11 @@ const cfg = resolveConfig({
 const ctx = new Context(cfg);
 
 describe('get files', () => {
-  it('pages', async () => {
+  it('pages files', async () => {
     await ctx.scanFiles();
-    const files = [...ctx.files.keys()].map(f => normalizePath(path.relative(cfg.root, f))).sort();
+    const files = [...ctx.pages.keys()].map(f => normalizePath(path.relative(cfg.root, f))).sort();
     expect(files).toMatchInlineSnapshot(`
       [
-        "src/pages-sub/about/index.vue",
-        "src/pages-sub/about/your.vue",
-        "src/pages-sub/index.vue",
         "src/pages/define-page/async-function.vue",
         "src/pages/define-page/function.vue",
         "src/pages/define-page/nested-function.vue",
@@ -28,6 +25,27 @@ describe('get files', () => {
         "src/pages/define-page/option-api.vue",
         "src/pages/define-page/yaml.vue",
         "src/pages/index/index.vue",
+      ]
+    `);
+  });
+
+  it('sub packages files', async () => {
+    await ctx.scanFiles();
+
+    const files: string[] = [];
+
+    for (const [, sub] of ctx.subPackages) {
+      for (const [file] of sub) {
+        files.push(normalizePath(path.relative(cfg.root, file)));
+      }
+    }
+
+    files.sort();
+    expect(files).toMatchInlineSnapshot(`
+      [
+        "src/pages-sub/about/index.vue",
+        "src/pages-sub/about/your.vue",
+        "src/pages-sub/index.vue",
       ]
     `);
   });
