e4e252c9744c76c7eecea88743fb4671172e8ed3
edwinhuish <edwinhuish@gmail.com>
2025-11-04 09:23:20 +0800

    types: 修正global声明

--- global.d.ts
+++ global.d.ts
@@ -1,8 +1,6 @@
-import { definePage } from '@uni-ku/pages-json';
-
-const _definePage = definePage;
+import type { definePage as DefinePage } from '@uni-ku/pages-json';
 
 // 全局声明 definePage 函数，使得用户无需导入即可使用
 declare global {
-  const definePage: typeof _definePage;
+  const definePage: typeof DefinePage;
 }
