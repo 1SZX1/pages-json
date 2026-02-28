3e4e8443d91d52c435a70918432354ea14c13919
xiaohe0601 <xiaohe0601@outlook.com>
2026-02-28 14:06:23 +0800

    fix: 修复 subPackages 的 root 为多级目录时错误

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -3,3 +3,7 @@ export function noop() {}
 export function sleep(ms: number) {
   return new Promise(resolve => setTimeout(resolve, ms));
 }
+
+export function slash(str: string) {
+  return str.replace(/\\/g, '/');
+}
